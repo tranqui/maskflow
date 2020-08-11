460bcf09dfbd4050de34c88a04091f62434b5afb
Joshua F. Robinson <joshuarrr@protonmail.com>
2020-08-11 15:14:00 +0100

    Improved output and plotting in multifibre, and fixed integration over fibre diameters for narrow distributions.

--- maskflow/multifibre.py
+++ maskflow/multifibre.py
@@ -71,10 +71,13 @@ class LogNormalFibreDistribution(FibreDistribution):
     def log_pdf(self):
         return lambda x: np.exp(-0.5*(x-self.mu)**2/self.sigma**2) / (self.sigma*np.sqrt(2*np.pi))
 
-    def average(self, f, n=30):
+    def average(self, f, norder=30, nsigma=3):
         if self.sigma == 0: return f(np.exp(self.mu))
         p = self.log_pdf
-        return integrate.fixed_quad(lambda d: p(d)*f(np.exp(d)), self.mu-3, self.mu+3, n=n)[0]
+        return integrate.fixed_quad(lambda d: p(d)*f(np.exp(d)),
+                                    self.mu - nsigma*self.sigma,
+                                    self.mu + nsigma*self.sigma,
+                                    n=norder)[0]
 
 if __name__ == '__main__':
     import argparse
@@ -102,20 +105,23 @@ if __name__ == '__main__':
 
     df_distribution = LogNormalFibreDistribution(np.log(args.fibre_diameter), args.fibre_sigma)
 
-    #from natsort import natsorted
     data = [read_yaml(path) for path in args.data_files]
     data = [d for d in data if 'lambda' in d]
     data = sorted(data, key=lambda d: d['fibre_radius'])
 
     alpha = data[0]['alpha']
     flow_speed = data[0]['flow_speed']
+    face_speed = flow_speed * (1 - alpha)
     K = data[0]['hydrodynamic_factor']
     particle_diameters = 2*np.array(data[0]['particle_radius'])
     sampled_fibre_diameters = 2*np.array([d['fibre_radius'] for d in data])
     sampled_lambdas = np.array([np.array(d['lambda']) for d in data])
 
+    if args.plot:
+        fig, (ax1,ax2) = plt.subplots(nrows=2, figsize=(3.375, 6))
+
+    Rmax = (1 / np.sqrt(alpha) - 1)
     for df,lam in zip(sampled_fibre_diameters, sampled_lambdas):
-        Rmax = (1 / np.sqrt(alpha) - 1)
         dpmax = df * Rmax
         #lam_max = df / np.sqrt(alpha)
         select = particle_diameters < dpmax
@@ -134,12 +140,80 @@ if __name__ == '__main__':
         lam = sampled_lambdas[:,i]
         logdf = np.log(sampled_fibre_diameters)
         lam_func = LambdaInterpolator(sampled_fibre_diameters, sampled_lambdas[:,i])
-        averaged_lambda[i] = df_distribution.average(lam_func) #lambda x: lam_func(1e6*x))
+        averaged_lambda[i] = df_distribution.average(lam_func)
 
     if args.plot:
-        plt.loglog(particle_diameters, averaged_lambda)
-        plt.xlabel('particle diameter $d_p$ (\si{\micro\meter})')
-        plt.ylabel(r'$\langle \lambda \rangle$')
+        ax2.loglog(particle_diameters, averaged_lambda)
+
+        from matplotlib.colors import LogNorm
+        lam_lim = [np.min(sampled_lambdas), np.max(sampled_lambdas)]
+        ax2.set_ylim(lam_lim)
+        norm = LogNorm(vmin=lam_lim[0], vmax=lam_lim[1])
+        Y,X = np.meshgrid(sampled_fibre_diameters, particle_diameters, indexing='ij')
+        ax1.pcolormesh(X, Y, sampled_lambdas, norm=norm)
+
+        # use the averaged lambda plot as the colour bar for the first plot
+        l = np.logspace(np.log10(lam_lim[0]), np.log10(lam_lim[1]), 101)
+        L,X = np.meshgrid(l, particle_diameters, indexing='ij')
+        ax2.pcolormesh(X, L, L, norm=norm, zorder=-1)
+
+        ax1.set_xscale('log')
+        ax1.set_yscale('log')
+        xlim = ax1.get_xlim()
+        ylim = ax1.get_ylim()
+
+        # Plot boundary of extrapolated region.
+        dpmax = sampled_fibre_diameters * Rmax
+        ax1.plot(dpmax, sampled_fibre_diameters, '--')
+
+        # Determine angle of rotation required to align text with line.
+        aspect_y = np.log10(ylim[1]) - np.log10(ylim[0])
+        aspect_x = np.log10(xlim[1]) - np.log10(xlim[0])
+        aspect = aspect_y / aspect_x
+        bbox = ax1.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
+        width, height = bbox.width, bbox.height
+        aspect /= height / width
+        angle = np.arctan2(aspect, 1) * (180/np.pi)
+
+        label_x = np.exp(np.average(np.log(dpmax[dpmax < xlim[1]])))
+        label_y = 0.75 * label_x / Rmax
+        ax1.text(label_x, label_y, 'extrapolated region', fontsize=10, rotation=angle,
+                 horizontalalignment='center',
+                 verticalalignment='center')
+
+        ax1.set_xlim(xlim)
+        ax2.set_xlim(xlim)
+
+        for ax in [ax1, ax2]: ax.set_xlabel('particle diameter $d_p$ (\si{\micro\metre})')
+        ax1.set_ylabel(r'fibre diameter $d_f$ (\si{\micro\metre})')
+        ax2.set_ylabel(r'$\langle \lambda \rangle$')
+
+        ax1.text(0.075, 0.9, r'$\lambda$', c='w',
+                 transform=ax1.transAxes,
+                 horizontalalignment='center',
+                 verticalalignment='center')
+
+        if args.fibre_sigma > 0:
+            ax3 = ax1.twiny()
+            df = sampled_fibre_diameters
+            p = df_distribution.pdf(df)
+            ax3.plot(p, df)
+            ax3.set_xticks([])
+            ax3.set_xlim([0, 4*np.max(p)])
+            ax3.invert_xaxis()
+            label_x, label_y = 1.1*np.max(p), df[np.argmax(p)]
+        else:
+            ax3 = ax1.twiny()
+            df = args.fibre_diameter
+            ax3.plot([0, 1], [df, df])
+            ax3.set_xticks([])
+            ax3.set_xlim([0, 4])
+            ax3.invert_xaxis()
+            label_x, label_y = 1.1, df
+
+        ax3.text(label_x, label_y, r'$p(d_f)$', fontsize=10,
+                 horizontalalignment='right',
+                 verticalalignment='center')
 
     fibre_number_density = 4*alpha/(np.pi*df_distribution.moment(2))
     penetration_length = (1 - alpha) / (fibre_number_density * averaged_lambda)
@@ -151,7 +225,7 @@ if __name__ == '__main__':
         plt.semilogx(particle_diameters, 1-P)
         plt.xlabel('particle diameter $d_p$ (\si{\micro\meter})')
         plt.ylabel('$1 - P$')
-        plt.title('$L = \SI{1}{\milli\meter}$')
+        plt.title('$L = \SI{%g}{\milli\meter}$' % (filter_thickness/1000))
 
     if args.yaml:
         # If enabled force YAML compliant output by removing whitespace at start of printed data.
@@ -166,9 +240,11 @@ if __name__ == '__main__':
     print('      fibre_diameter:', args.fibre_diameter)
     print('         fibre_sigma:', args.fibre_sigma)
     print('               alpha:', alpha)
+    print('fibre_number_density:', fibre_number_density)
+    print('          face_speed:', face_speed)
     print('          flow_speed:', flow_speed)
     print('  particle_diameters:', particle_diameters)
     print('              lambda:', averaged_lambda)
     print('  penetration_length:', penetration_length)
 
-    plt.show()
+    if args.plot: plt.show()
