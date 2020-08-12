1628ee2d36bfa2128cd7d3c5f66a7591410645af
Joshua F. Robinson <joshuarrr@protonmail.com>
2020-08-12 13:48:24 +0100

    Refactored setup script to obtain the version number directly from the git repo.

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import setuptools
+import subprocess
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+version = subprocess.check_output(["git", "describe", "--tags"]).decode('ascii').strip()
+
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 requirements = []
 required_modules = ["numpy", "scipy", "natsort"]
@@ -13,18 +16,23 @@ for module in required_modules:
 
 setuptools.setup(
     name="maskflow",
-    version="v1.0.0",
-    author="joshuarrr",
-    install_requires=requirements,
-    author_email="joshuarrr@protonmail.com",
+    version=version,
+    license='GNU General Public License v3.0',
+
+    author="Joshua F. Robinson",
+    author_email='joshua.robinson@bristol.ac.uk',
+
+    url='https://github.com/tranqui/maskflow.git',
     description="Code to evaluate filtration properties of face masks.",
     long_description=long_description,
-    license='GPLv3',
     long_description_content_type="text/markdown",
+
     python_requires='>=3',
     packages=setuptools.find_packages(),
     package_data = {'': ['*.csv']},
     include_package_data = True,
+    install_requires=requirements,
+
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: Linux",
